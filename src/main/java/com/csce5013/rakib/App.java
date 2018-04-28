fa08d83344ef986f29deed0fbf7a4afa9476d2bc
Md Rakib Shahriar <rakib.shahriar@csebuet.org>
2018-04-27 19:31:06 -0500

    First commit

---
+++ src/main/java/com/csce5013/rakib/App.java
@@ -0,0 +1,13 @@
+package com.csce5013.rakib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
