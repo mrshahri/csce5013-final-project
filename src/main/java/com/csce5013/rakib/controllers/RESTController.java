61d43fd6eaff9408e475c2ae177f75bc2d723113
admin <mrshahri@email.uark.edu>
2018-04-29 17:09:05 -0500

    Now Machine Stauses are read from PiServer

--- src/main/java/com/csce5013/rakib/controllers/RESTController.java
+++ src/main/java/com/csce5013/rakib/controllers/RESTController.java
@@ -1,6 +1,13 @@
 package com.csce5013.rakib.controllers;
 
 import com.csce5013.rakib.models.InstanceModel;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -8,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @RequestMapping("/webservices")
@@ -19,11 +29,34 @@ public class RESTController {
     private final static org.slf4j.Logger logger = LoggerFactory.getLogger(RESTController.class);
 
     @RequestMapping(value = "/instances", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<InstanceModel> getInstances() {
+    public List<InstanceModel> getInstances() throws IOException {
 
         // HACK ALERT: Couldn't fix Spring Boot AWSManager initialization, so using this hack
         awsManager.connectAWSIfNot();
+        List<InstanceModel> instances = awsManager.getListOfCreatedInstances();
 
-        return awsManager.getListOfCreatedInstances();
+        // Update status of machines from PiServer
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet getRequest = new HttpGet("http://localhost:8080/statuses");
+        getRequest.addHeader("accept", "application/json");
+        CloseableHttpResponse response = httpClient.execute(getRequest);
+        try {
+            if (response.getStatusLine().getStatusCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            JsonReader reader = new JsonReader(br);
+            Gson gson = new Gson();
+            JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+            for (InstanceModel instance : instances) {
+                instance.setMachineStatus(jsonObject.get(instance.getMachineId()).getAsString());
+            }
+        } catch (IOException e) {
+            logger.error("Failed to fetch machine status {}", e);
+        } finally {
+            response.close();
+        }
+
+        return instances;
     }
 }
